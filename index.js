62f657882a2e4cf66a6499884d702bcbfa68b1f7
Hypermona <mohans8050@gmail.com>
2022-05-23 20:04:31 +0530

    testing download

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// https://stackoverflow.com/questions/10046039/node-js-send-file-in-response
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/download", (req, res) => {
+  const file = `${__dirname}/simple.rar`;
+  res.download(file);
 });
 
 app.listen(port, () => {
